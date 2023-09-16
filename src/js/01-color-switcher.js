21459a8ae5372c700b826382750d9c8aad34c7ad
Dmytro Zinchuk <127327533+dmytro2104z@users.noreply.github.com>
2023-09-16 14:53:36 +0200

    01commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+function startChangingColor() {
+  if (intervalId) return;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+}
+
+function stopChangingColor() {
+  if (!intervalId) return;
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false;
+}
+
+startButton.addEventListener('click', startChangingColor);
+stopButton.addEventListener('click', stopChangingColor);
